92316a4b15d14b7e30233ad7f6a893395e963737
Gabriel Aguilar <gabe.p.aguilar@gmail.com>
2020-08-30 07:42:30 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@ def fizzbuzz(int)
     if int % 3 == 0 && int % 5 == 0
       return "Fizzbuzz"
 
-    elsif condition
+    elsif (int)
        int % 3 == 0
        return "Fizz"
 
-    elsif condition
+    elsif (int)
        int % 5 == 0
       return "Buzz"
     end
