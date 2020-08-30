d5cdbe6539dbfdfc821ca1a84c5d437b9a52e86b
Gabriel Aguilar <gabe.p.aguilar@gmail.com>
2020-08-30 07:49:39 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,4 @@
-def fizzbuzz(int)
-    if int % 3 == 0 && int % 5 == 0
-      "Fizzbuzz"
-
-    elsif int
-       int % 3 == 0
-       "Fizz"
-
-    elsif int
-       int % 5 == 0
-      "Buzz"
-
-    else int
-    end
-
+def fizzbuzz(number)
+   if number % 3 == o
+     "fizz"
 end
