1ff72d1d02565f7195aed93fac5b4df23f4b8680
Gabriel Aguilar <gabe.p.aguilar@gmail.com>
2020-08-30 07:22:13 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,14 @@
-def fizzbuzz
-
+def fizzbuzz(int)
+  if int % 3 == 0
+    "Fizz"
+  elsif (int)
+    int % 5 == 0
+    "Buzz"
+  elsif (int)
+    if int % 3 or 5
+    "Fizzbuzz"
+else (int)
+  if int == 4
+  "nil"
 end
+
